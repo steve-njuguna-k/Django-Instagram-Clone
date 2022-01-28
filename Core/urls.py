0f9fbc1f73735e076d9cfe8ab773a719464f8d92
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-28 20:55:30 +0300

    Added config for images upload

--- Core/urls.py
+++ Core/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from Core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('App.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
