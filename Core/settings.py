f668dd8e17ca4e04c46d438c87258b9766d9071c
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-28 12:23:03 +0300

    Added register page & styled form

--- Core/settings.py
+++ Core/settings.py
@@ -126,10 +126,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
 
 # Media Files
