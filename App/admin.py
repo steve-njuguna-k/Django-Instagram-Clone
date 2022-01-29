de5aec9bf195a8bc410b5e70d59aba0b5a7ade2f
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-29 13:39:34 +0300

    Changed logo

--- App/admin.py
+++ App/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Posts, Profile
+from .models import ImagePost, Profile, County, Category
 
 # Register your models here.
-admin.site.register(Posts) 
+admin.site.register(ImagePost) 
+admin.site.register(County) 
+admin.site.register(Category) 
+
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('profile_image', 'bio', 'date_published', 'date_updated')
+    list_display = ('profile_image', 'bio', 'date_created', 'date_updated')
     search_fields = []
-    readonly_fields=('date_published', 'date_updated')
+    readonly_fields=('date_created', 'date_updated')
     
 admin.site.register(Profile, ProfileAdmin)
