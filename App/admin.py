9f31917614e9c73c462fa9be408b138a0de198bc
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-05 15:44:07 +0300

    Fixed errors in admin.py

--- App/admin.py
+++ App/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from .models import ImagePost, Profile, County, Category
+from .models import Post, Profile
 
 # Register your models here.
-admin.site.register(ImagePost) 
-admin.site.register(County) 
-admin.site.register(Category) 
+admin.site.register(Post) 
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('profile_image', 'bio', 'date_created', 'date_updated')
