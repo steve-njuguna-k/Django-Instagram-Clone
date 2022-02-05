34c293a71110bd3a8f72e1c0e0c015ff7cdcb0c1
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-05 16:00:57 +0300

    Added Email setup

--- Core/settings.py
+++ Core/settings.py
@@ -151,3 +151,12 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
+
+#Email Setup
+EMAIL_FROM_USER = os.environ.get("EMAIL_FROM_USER")
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
